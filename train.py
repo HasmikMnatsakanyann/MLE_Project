08a6f2df63fef09ad7053c17e0e1a4d8cf6a0c80
HasmikMnatsakanyann <130249430+HasmikMnatsakanyann@users.noreply.github.com>
2023-12-17 01:30:12 +0400

    Update train.py

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from config import ModelConfig
 
 def train_model(X_train, y_train):
     dtrain = xgb.DMatrix(X_train, label=y_train)
-    bst = xgb.train(ModelConfig.XGB_PARAMS, dtrain, ModelConfig.num_rounds)
+    bst = xgb.train(ModelConfig.XGB_PARAMS, dtrain, 900)
 
+    bst.save_model('models/your_model.json')
     return bst
