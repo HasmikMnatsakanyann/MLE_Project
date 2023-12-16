db268840f9f66a24dc9f58d3586165e101022da2
HasmikMnatsakanyann <130249430+HasmikMnatsakanyann@users.noreply.github.com>
2023-12-17 01:37:48 +0400

    Update test.py

--- test.py
+++ test.py
@@ -8,3 +8,14 @@ def test_model(model, X_test, y_test):
 
     mae = mean_absolute_error(preds,y_test)
     return mae
+
+def test_loaded_model(model_path, X_test, y_test):
+    # Load the saved model
+    loaded_model = xgb.Booster()
+    loaded_model.load_model(model_path)
+
+    dtest = xgb.DMatrix(X_test, label=y_test)
+    preds = loaded_model.predict(dtest)
+
+    mae = mean_absolute_error(preds,y_test)
+    return mae
