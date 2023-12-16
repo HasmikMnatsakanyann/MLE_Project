f447da0dfdfee8e3adb3630f4342be505e8d329f
HasmikMnatsakanyann <130249430+HasmikMnatsakanyann@users.noreply.github.com>
2023-12-17 01:29:09 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from preprocessing import preprocess_data
+from preprocessing import data_preprocess
 from train import train_model
 from test import test_model
 from config import DataConfig
@@ -10,15 +10,21 @@ if __name__ == "__main__":
     data_path = "ARM_Cars.csv"
     data = pd.read_csv(data_path)
 
-    # Data preprocessing
-    X, y = preprocess_data(data)
+    X = data.drop(DataConfig.TARGET_COL,axis=1)
+    y = data[DataConfig.TARGET_COL]
 
     # Split data into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True, random_state=DataConfig.RANDOM_SEED)
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, shuffle=True, random_state=DataConfig.RANDOM_SEED)
+    
+    X_train = pd.DataFrame(X_train, columns = data.drop('Price ($)',axis=1).columns)
+    X_valid = pd.DataFrame(X_valid, columns = data.drop('Price ($)',axis=1).columns)
+
+    # Data preprocessing
+    X_train, X_valid = data_preprocess(X_train,X_valid)
 
     # Train the model
     model = train_model(X_train, y_train)
 
     # Test the model
-    mae = test_model(model, X_test, y_test)
+    mae = test_model(model, X_valid, y_valid)
     print(f'Mean Absolute Error on Test Set: {mae}')
