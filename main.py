2000f4f49537df8dbc25cb174dfc2556c28c719b
HasmikMnatsakanyann <130249430+HasmikMnatsakanyann@users.noreply.github.com>
2023-12-17 13:07:22 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from preprocessing import data_preprocess
+from preprocessing import data_preprocess,change_cat_type
 from train import train_model
-from test import test_model,test_loaded_model
+from test import test_model,test_loaded_model, loaded_model_pred
 from config import DataConfig
 
 if __name__ == "__main__":
@@ -25,11 +25,22 @@ if __name__ == "__main__":
     # Train the model
     model = train_model(X_train, y_train)
 
-    #Test loaded model
-    model_path = 'your_model.json'
-    mae = test_loaded_model(model_path, X_valid, y_valid)
-    print(f'Mean Absolute Error on Validation Set: {mae}')
-
     # Test the model
     mae = test_model(model, X_valid, y_valid)
     print(f'Mean Absolute Error on Validation Set: {mae}')
+
+    ########################################################Test on a single point##########################################################
+    # #Write input like in the following order ['Car', 'Date Posted', 'Year', 'Mileage', 'Vehicle Type', 'Transmission','Wheel left/right', 'Color']
+    # sample = ['Chevrolet Cruze', '13.02.2020', 2015, 30000.0, 'Sedan', 'Automatic','Left', 'Black']
+    # X_test = pd.DataFrame([sample], columns = ['Car', 'Date Posted', 'Year', 'Mileage', 'Vehicle Type', 'Transmission','Wheel left/right', 'Color'])
+    # X_test = change_cat_type(X_test)
+    # X_train,X_test = data_preprocess(X_train, X_test)
+
+    # model_path = 'models/your_model.json'
+    # preds = loaded_model_pred(model_path, X_test)
+    # print(f'Predition on Test Set: {preds}')
+
+    ########################################################Test loaded model###############################################################
+    # model_path = 'models/your_model.json'
+    # mae = test_loaded_model(model_path, X_valid, y_valid)
+    # print(f'Mean Absolute Error on Validation Set: {mae}')
